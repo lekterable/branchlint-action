42c1555bb1f738480ba3de8111f02e7082668faa
Kornel Dubieniecki <lekterable@gmail.com>
2020-04-14 21:35:27 +0200

    feat: validate branch name

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('Init')
+const core = require('@actions/core')
+const github = require('@actions/github')
+const RegexParser = require('regex-parser')
+
+const isRegex = (input = '') => /^\/.*\/[gimsuy]?$/.test(input)
+const validateName = (patterns = [], branchName = '') =>
+  patterns.some(pattern =>
+    isRegex(pattern)
+      ? RegexParser(pattern).test(branchName)
+      : pattern === branchName
+  )
+
+const run = async () => {
+  try {
+    const allowed = core.getInput('allowed', { required: true })
+    const branchName = github.context.payload.pull_request.head.ref
+    const patterns = allowed.split('\n')
+
+    const isCorrect = validateName(patterns, branchName)
+
+    if (!isCorrect) throw new Error('Your branch name is not allowed')
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+run()
